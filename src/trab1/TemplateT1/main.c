75487b1d686e6ed67d610ce906369eff2e6bafd4
João Barata Oliveira <jmdbo@hotmail.com>
2015-03-24 17:10:50 +0000

    Updates

--- src/trab1/TemplateT1/main.c
+++ src/trab1/TemplateT1/main.c
@@ -1,5 +1,6 @@
 #include <p24fxxxx.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "uartcom.h"
 
@@ -15,8 +16,8 @@ _CONFIG2( FCKSM_CSDCMD & OSCIOFNC_OFF & POSCMOD_HS & FNOSC_PRI)
 
 
 int initADC(){ //potenciometro
-	//AD1PCFG = 0xFFDF; 		// AN5 as analog, all other pins are digital
-	AD1PCFG = 0xFFFB;		//AN2
+	AD1PCFG = 0xFFDF; 		// AN5 as analog, all other pins are digital
+	//AD1PCFG = 0xFFFB;		//AN2
 	AD1CON1 = 0x0000; 		// SAMP bit = 0 ends sampling
 							// and starts converting
 	AD1CHS = 0x0005; 		// Connect AN5 as CH0 input
@@ -40,48 +41,54 @@ int main(void)
 
 	long int i = 0;
 	int ADCValue=0;
+	char buff[40];
 
 	while ( 1 ){
-
-		PORTAbits.RA0 = 1; //liga led
-		if(ADCValue>=0 || ADCValue<=10){
-			for( i = 0 ; i < 100000 ; i++){};
-		}
-		if(ADCValue>10 || ADCValue <=20){
-			for( i = 0 ; i < 90000 ; i++){};
-		}
-		if(ADCValue>20 || ADCValue <=30){
-			for( i = 0 ; i < 80000 ; i++){};
-		}
-		if(ADCValue>30 || ADCValue <=40){
-			for( i = 0 ; i < 70000 ; i++){};
-		}
-		if(ADCValue>40 || ADCValue <=50){
-			for( i = 0 ; i < 60000 ; i++){};
-		}
-		if(ADCValue>50 || ADCValue <=60){
-			for( i = 0 ; i < 50000 ; i++){};
-		}
-		if(ADCValue>60 || ADCValue <=70){
-			for( i = 0 ; i < 40000 ; i++){};
-		}
-		if(ADCValue>70 || ADCValue <=80){
-			for( i = 0 ; i < 30000 ; i++){};
-		}
-		if(ADCValue>80 || ADCValue <=90){
-			for( i = 0 ; i < 20000 ; i++){};
-		}
-		if(ADCValue>90 || ADCValue <=100){
-			for( i = 0 ; i < 10000 ; i++){};
-		}
-		PORTAbits.RA0 = 0; // desliga led
-		
 		if(U2STAbits.URXDA){
 			char rec = getcharUART();
 			if(rec=='T')
 				putstringUART("Received String\n");
+			if(rec=='P'){
+				sprintf(buff,"Potentiometer: %d\n", ADCValue);
+				putstringUART(buff);
+			}
 		}	
 		if ( !PORTDbits.RD6){
+			/*
+			PORTAbits.RA0 = 1; //liga led
+			if(ADCValue>=0 || ADCValue<=10){
+				for( i = 0 ; i < 100000 ; i++){};
+			}
+			if(ADCValue>10 || ADCValue <=20){
+				for( i = 0 ; i < 90000 ; i++){};
+			}
+			if(ADCValue>20 || ADCValue <=30){
+				for( i = 0 ; i < 80000 ; i++){};
+			}
+			if(ADCValue>30 || ADCValue <=40){
+				for( i = 0 ; i < 70000 ; i++){};
+			}
+			if(ADCValue>40 || ADCValue <=50){
+				for( i = 0 ; i < 60000 ; i++){};
+			}
+			if(ADCValue>50 || ADCValue <=60){
+				for( i = 0 ; i < 50000 ; i++){};
+			}
+			if(ADCValue>60 || ADCValue <=70){
+				for( i = 0 ; i < 40000 ; i++){};
+			}
+			if(ADCValue>70 || ADCValue <=80){
+				for( i = 0 ; i < 30000 ; i++){};
+			}
+			if(ADCValue>80 || ADCValue <=90){
+				for( i = 0 ; i < 20000 ; i++){};
+			}
+			if(ADCValue>90 || ADCValue <=100){
+				for( i = 0 ; i < 10000 ; i++){};
+			}
+			PORTAbits.RA0 = 0; // desliga led
+			
+			*/
 			PORTAbits.RA5 = 1; //led
 			for( i = 0 ; i < 20 ; i++){};
 			PORTAbits.RA0 = 1; //liga led
